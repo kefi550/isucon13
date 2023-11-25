3e88af3bf49ceea4281c272238ff4024193a2389
bebeken <26348722+bebeken@users.noreply.github.com>
2023-11-25 11:27:14 +0900

    インデックス追加 2876点

--- webapp/sql/initdb.d/10_schema.sql
+++ webapp/sql/initdb.d/10_schema.sql
@@ -104,3 +104,28 @@ CREATE TABLE `reactions` (
   `emoji_name` VARCHAR(255) NOT NULL,
   `created_at` BIGINT NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+create index icons_user_id on `icons` (`user_id`);
+create index themes_user_id on `themes` (`user_id`);
+create index livestreams_id on `livestreams` (`id` DESC);
+create index livestreams_user_id on `livestreams` (`user_id`);
+create index livestreams_playlist_url on `livestreams` (`playlist_url`);
+create index livestreams_thumbnail_url on `livestreams` (`thumbnail_url`);
+create index livestream_tags_livestream_id on `livestream_tags` (`livestream_id` DESC);
+create index livestream_tags_tag_id on `livestream_tags` (`tag_id`);
+create index livestream_tags_created_at on `livestream_tags` (`created_at` DESC);
+create index livestream_viewers_history_user_id on `livestream_viewers_history` (`user_id`);
+create index livestream_viewers_history_livestream_id on `livestream_viewers_history` (`livestream_id`);
+create index livecomments_user_id on `livecomments` (`user_id`);
+create index livecomments_livestream_id on `livecomments` (`livestream_id`);
+create index livecomments_created_at on `livecomments` (`created_at` DESC);
+create index livecomment_reports_user_id on `livecomment_reports` (`user_id`);
+create index livecomment_reports_livestream_id on `livecomment_reports` (`livestream_id`);
+create index livecomment_reports_livecomment_id on `livecomment_reports` (`livecomment_id`);
+create index ng_words_user_id on `ng_words` (`user_id`);
+create index ng_words_livestream_id on `ng_words` (`livestream_id`);
+create index ng_words_created_at on `ng_words` (`created_at` DESC);
+create index reactions_user_id on `reactions` (`user_id`);
+create index reactions_livestream_id on `reactions` (`livestream_id`);
+create index reactions_emoji_name on `reactions` (`emoji_name` DESC);
+create index reactions_created_at on `reactions` (`created_at` DESC);
