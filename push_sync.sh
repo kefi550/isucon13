3b058abb5ee47899051ca87794c021f97982e0a8
ningen <msg1801022@gmail.com>
2023-11-25 11:59:41 +0900

    isu3にもpushする

--- push_sync.sh
+++ push_sync.sh
@@ -10,10 +10,10 @@ ssh isu1 sudo logrotate -f /etc/logrotate.conf
 ssh isu1 sudo systemctl restart isupipe-node.service
 #rsync -vr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu2:~/isucari/webapp/nodejs/ 
 #ssh isu2 sudo systemctl restart isucari.nodejs.service
-#rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu3:~/isucari/webapp/nodejs/ 
-#ssh isu3 sudo systemctl restart isucari.nodejs.service
-#rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu2:~/isucari/webapp/sql/
-#rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu3:~/isucari/webapp/sql/
+rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu3:~/isucari/webapp/nodejs/ 
+ssh isu3 sudo systemctl restart isucari.nodejs.service
+rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu2:~/isucari/webapp/sql/
+rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu3:~/isucari/webapp/sql/
 
 #rsync -vr --exclude='' "${LOCAL_PATH}/nginx/isu1/" isu1:/etc/nginx/
 #rsync -vr --exclude='' "${LOCAL_PATH}/nginx/isu2/" isu2:/etc/nginx/
